24380d778f8b470198f4919bc81fd91748cd6edf
mun <t3hmun@gmail.com>
2016-09-14 19:22:16 +0100

    Start of new publish + intellij settings.

--- src/publish.js
+++ src/publish.js
@@ -4,3 +4,13 @@
 const helpers = require('./helper');
 const processors = require('./processors');
 
+const pubDir = '../t3hmun.github.io';
+
+const topLevelConfig = {
+    dir : '../site',
+    dirPredicate: ()=>false, // No recursion
+    filePredicate: ()=> true,
+    processors:{
+
+    }
+};
